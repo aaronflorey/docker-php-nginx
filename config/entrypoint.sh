e47a6cdcacdc5d002ad4ef4ce0cafcdb9ede83aa
Aaron <azza@jcaks.net>
2020-07-29 11:39:37 +1000

    ignore output from optimize

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -2,5 +2,5 @@
 cd /var/www/html
 php artisan storage:link 2&1> /dev/null
 #php artisan migrate --force
-php artisan optimize
+php artisan optimize 2&1> /dev/null
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
