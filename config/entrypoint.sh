49a04aff0c1bc6ad5e62ac9c5fb2a28f4323c96a
Aaron Florey <azza@jcaks.net>
2020-08-07 11:16:01 +1000

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/html
-php artisan storage:link > /dev/null 2>&1
+#php artisan storage:link > /dev/null 2>&1
 php artisan migrate --force > /dev/null 2>&1
-php artisan optimize > /dev/null 2>&1
+#php artisan optimize > /dev/null 2>&1
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
